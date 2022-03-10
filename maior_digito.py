f3eea33da991c5d54c9f0409390e69879dc2563d
N4cci <92125736+N4cci@users.noreply.github.com>
2022-03-10 20:56:06 -0300

    Update maior_digito.py

--- maior_digito.py
+++ maior_digito.py
@@ -6,12 +6,14 @@ digito do mesmo.
 * while: 1
 * int: 1
 """
+#m = m // 10
 
+n = int(input("n: "))
 m = 0
-m = m // 10
-m = max(unidade, m)
+
 while n > 0:
-    n = int(input("n: "))
-    n = n // 10
-    print(f"maior digito = {m}")
     unidade = n % 10
+    n = n // 10
+    m = max(unidade, m)
+    
+print(f"maior digito = {m}")
