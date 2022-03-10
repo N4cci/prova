192b2d1cc4c50ee4e6226818fc52e9ffac2cb5c5
N4cci <92125736+N4cci@users.noreply.github.com>
2022-03-10 20:51:06 -0300

    Update zeros.py

--- zeros.py
+++ zeros.py
@@ -6,13 +6,13 @@ zeros à esquerda do mesmo.
 * while: 1
 * str: 1
 """
+#while number[0] == "0":
+#while number == "0":
+#number = int(number)
 
-while number == "0":
+number = input("n: ")
+n = 0
+while number.startswith("0"):
     n += 1
-    n = 0
-    number = input("n: ")
-    while number[0] == "0":
-        number = int(number)
-        number = number[1:]
-        while number.startswith("0"):
-            print(f"numero de zeros = {n}")
+    number = number[1:]
+print(f"numero de zeros = {n}")
